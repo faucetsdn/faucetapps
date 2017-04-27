aff5fcf79429dd58c149f0e858fb46f8668c1a99
shivarammysore <shivaram.mysore@gmail.com>
2017-04-27 16:07:16 -0700

    Packaging updates and remove stale javascript file reference in index.html

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ from setuptools import setup
 
 setup_dir = os.path.dirname(__file__)
 readme_contents = open(os.path.join(setup_dir, 'README.rst')).read()
-faucet_version = re.match(r'.+version: ([0-9\.]+)', readme_contents).group(1)
-os.environ["PBR_VERSION"] = faucet_version
+faucetapps_version = re.match(r'.+version: ([0-9\.]+)', readme_contents).group(1)
+os.environ["PBR_VERSION"] = faucetapps_version
 
 setup(
-    name='faucet',
+    name='faucetapps',
     setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
     pbr=True
 )
